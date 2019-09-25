92e05b65eb854eccdefd7270d568ec3ace9e0c72
brandonbouley <brandonbouley@billericak12.com>
2019-09-25 11:30:36 -0400

    chese burger

--- src/MathEx.java
+++ src/MathEx.java
@@ -1,4 +1,60 @@
+
 public class MathEx {
 
+    public static int swap(int x) {
 
-}
+        int a=(x%10);
+        int b=(x%100)/10;
+        int c=(x%1000)/100; //isolate the digits in terms of ones
+
+        a*=10;
+        c*=100; //convert the digit from the ones to tens and from ones to hundreds
+        int d= a+b+c; //put frankenstein's number back together
+
+        return d;
+    }
+    public static int timeLeft(int curHour, int curMin, int depHour, int depMin) {
+        int a= curHour+12;
+        int b= depHour+12;
+        int remHour=b-a;
+        System.out.println(remHour);
+
+
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        int a=swap(123);
+        int b=swap(456);
+        int c=swap(789);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        int d=timeLeft(1,0,11,0);
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    }
